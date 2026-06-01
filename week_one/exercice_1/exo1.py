1113b7022ff3d6d1ea6ce3821af433852798ff52
nassaigael <hei.nassai@gmail.com>
2026-06-01 10:30:18 +0300

    feat(exo1, exo2): add initial variable type examples
    * create exo1.py with examples of different variable types
    * create exo2.py demonstrating type conversion
    * include print statements to display variable types and values

---
+++ week_one/exercice_1/exo1.py
@@ -0,0 +1,9 @@
+var_int = 156
+var_float = 3.14
+var_str = "Hello, World!"
+var_bool = False
+
+print(type(var_int))
+print(type(var_float))
+print(type(var_str))
+print(type(var_bool))   
---
+++ week_one/exercice_1/exo2.py
@@ -0,0 +1,5 @@
+my_int = int(3.14)
+my_str = str(156)
+
+print(my_int)
+print(my_str)
