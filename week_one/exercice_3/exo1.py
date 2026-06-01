76872695d4c5d48ae373dd48a4259ebf537368e0
nassaigael <hei.nassai@gmail.com>
2026-06-01 12:09:23 +0300

    feat(exo1): add multiplication table generator
    * Implemented a multiplication table generator for numbers 1 to 12.
    * Each table displays the multiplication results for numbers 1 to 10.
    * Added blank lines for better readability between tables.

---
+++ week_one/exercice_3/exo1.py
@@ -0,0 +1,7 @@
+for i in range(1, 13):
+    print(f"Multiplication table for {i}:")
+
+    for j in range(1, 11):
+        print(f"{i} x {j} = {i * j}")
+
+    print()  # Print a blank line after each multiplication table
