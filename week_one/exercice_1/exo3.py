8ecc4bcec150d36821c62a54e37ce866bd59e282
nassaigael <hei.nassai@gmail.com>
2026-06-01 10:36:02 +0300

    feat(exo3): add rectangle area and perimeter calculations
    * Introduced variables for length and width of a rectangle.
    * Calculated the area and perimeter based on the defined dimensions.
    * Added print statements to display the results for area and perimeter.

---
+++ week_one/exercice_1/exo3.py
@@ -0,0 +1,8 @@
+longueur = 5
+largeur = 3
+
+aire = longueur * largeur
+perimetre = 2 * (longueur + largeur)
+
+print("L'aire du rectangle est :", aire)
+print("Le périmètre du rectangle est :", perimetre)
