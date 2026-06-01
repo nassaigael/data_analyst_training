40e7e08d96954a02fe308455c228c1fa7767678b
nassaigael <hei.nassai@gmail.com>
2026-06-01 11:54:49 +0300

    feat(exo2): add number classification logic
    * Introduced logic to classify a number as positive, negative, or zero.
    * Added functionality to determine if the number is even or odd.
    * Implemented print statement to display the classification result.

---
+++ week_one/exercice_2/exo2.py
@@ -0,0 +1,5 @@
+number = -8;
+
+result = ("positif" if number > 0 else "négatif" if number < 0 else "nul") + " et " + ("pair" if number % 2 == 0 else "impair")
+
+print(f"le nombre {number} est {result}")
