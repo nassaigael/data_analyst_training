6c9171c370f595637079a36ca44ffbc5c3ea19c6
nassaigael <hei.nassai@gmail.com>
2026-06-01 16:09:40 +0300

    feat(exo4): add reverse functionality for a list of programming languages
    * Introduced a new script to demonstrate two methods of reversing a list:
      - Using slicing to create a reversed copy of the list.
      - Using the built-in reverse method to modify the original list in place.
    * This addition provides a practical example for understanding list manipulation
      in Python.

---
+++ week_one/day_02/exercice_1/exo4.py
@@ -0,0 +1,7 @@
+words = ["python", "java", "javascript", "c", "rust", "go", "swift", "ruby"]
+
+result_with_slice = words[::-1]
+words.reverse()
+
+print("Result with slice: ", result_with_slice)
+print("Result with reverse: ", words)
