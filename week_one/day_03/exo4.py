b9ef731d80a6246a81a70420537c9e25771a3a6c
nassaigael <hei.nassai@gmail.com>
2026-06-03 13:24:51 +0300

    feat(json): add news JSON file and loading script
    * Create a new JSON file for news data with sample content.
    * Implement a Python script to load JSON data from an input file
      and dump it into the newly created news JSON file.
    * Ensure the script handles file reading and writing with proper
      encoding.

---
+++ week_one/day_03/exo4.py
@@ -0,0 +1,21 @@
+import json
+
+output_file_path = './data/json/news.json'
+
+def load_json_and_dump_on_news_json(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4)
+
+def load_json(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+input_file_path = './data/json/input.json'
+
+load_json_and_dump_on_news_json(input_file_path)
+
+
+print(load_json(output_file_path))
