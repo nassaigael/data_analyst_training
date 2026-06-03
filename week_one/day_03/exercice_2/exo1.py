435c3c63fd6a8dc05845ead5c7421dfcb5c0d237
nassaigael <hei.nassai@gmail.com>
2026-06-03 13:43:10 +0300

    feat(exo1): add divide_by_zon_with_exceptions function
    * Implemented a function to perform division with exception handling.
    * Added error messages for division by zero and type errors.
    * Included example usage to demonstrate function behavior.

---
+++ week_one/day_03/exercice_2/exo1.py
@@ -0,0 +1,13 @@
+def divide_by_zon_with_exceptions(a, b):
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        return "Error: Division by zero is not allowed."
+    except TypeError:
+        return "Error: Both inputs must be numbers."
+    
+# Example usage:
+print(divide_by_zon_with_exceptions("22", 2))
+print(divide_by_zon_with_exceptions(22, 0))
+print(divide_by_zon_with_exceptions(22, 2))
