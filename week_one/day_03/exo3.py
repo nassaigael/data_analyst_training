36452ef176fbca9fcfe81077db20824747de2b40
nassaigael <hei.nassai@gmail.com>
2026-06-03 13:09:31 +0300

    feat(exo3): add JSON configuration loading and output
    * Implemented a function to load configuration from a JSON file.
    * Extracted database connection details, analysis thresholds, and logging
      settings from the loaded configuration.
    * Added print statements to display the loaded configuration values for
      verification.

---
+++ week_one/day_03/exo3.py
@@ -0,0 +1,31 @@
+import json 
+file_json_path = 'data/json/config.json'
+
+def load_json(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        config = json.load(file)
+        return config
+    
+config = load_json(file_json_path)
+
+
+db_host =config['database']['host']
+db_port = config['database']['port']
+db_name = config['database']['name']
+
+username = config['database']['credentials']['username']
+print(f"Username: {username}")
+
+passing_score = config['analysis']['thresholds']['passing']
+honors_score = config['analysis']['thresholds']['honors']
+print(f"Thresholds: Passing={passing_score}, Honors={honors_score}")
+
+output_format = config['analysis']['output']['format']
+output_dir = config['analysis']['output']['directory']
+print(f"Output: {output_format} -> {output_dir}")
+
+log_level = config['logging']['level']
+log_file = config['logging']['file']
+print(f"Logging: {log_level} -> {log_file}")
+
+print(f"Database : {db_host}:{db_port}/{db_name}")
