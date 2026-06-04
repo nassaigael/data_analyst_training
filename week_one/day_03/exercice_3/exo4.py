1ed80eb090ce4ee4665b4b85f7b4c6afe386a8ad
nassaigael <hei.nassai@gmail.com>
2026-06-04 16:59:30 +0300

    feat(exo4): add performance comparison for Python and NumPy arrays
    * Implemented a script to compare the performance of summing a large
      Python list and a NumPy array.
    * Generated a random integer array using both Python's built-in
      functions and NumPy.
    * Measured and printed the duration taken for each summation to
      highlight the performance difference.

---
+++ week_one/day_03/exercice_3/exo4.py
@@ -0,0 +1,24 @@
+import numpy as np
+import time
+import random
+
+list_size = 1_000_000
+
+python_array = [random.randint(1, list_size) for _ in range(list_size)]
+print("THIS IS THE PYTHON ARRAY")
+start_time = time.time()
+python_array_sum = sum(python_array)
+pythoon_duration = time.time() - start_time
+print(f"Python array sum: {python_array_sum}, duration: {pythoon_duration:.6f} seconds")
+
+print("\n" + "="*50 + "\n")
+
+numpy_array = np.random.randint(1, list_size, size=list_size)
+print("THIS IS THE NUMPY ARRAY")
+start_time = time.time()    
+numpy_array_sum = np.sum(numpy_array)
+numpy_duration = time.time() - start_time
+print(f"Numpy array sum: {numpy_array_sum}, duration: {numpy_duration:.6f} seconds")
+
+
+print(f"Numpy is {pythoon_duration / numpy_duration:.2f} times faster than Python for summing an array of size {list_size}.")
