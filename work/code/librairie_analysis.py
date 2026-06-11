01fc26056eaf23da1257f7d4a64ab2611f45ad6f
nassaigael <hei.nassai@gmail.com>
2026-06-11 15:51:54 +0300

    feat(librairie_analysis): enhance book sales analysis with new functions
    - Added `get_store_with_highest_sale_children_book` to identify stores excelling in children's book sales.
    - Added `get_book_with_bad_revenue` to detect books with the lowest revenue.
    - Improved logic and formatting in existing functions for clarity and performance.

--- work/code/librairie_analysis.py
+++ work/code/librairie_analysis.py
@@ -1,9 +1,40 @@
 import pandas as pd
 
 
+# 1 Quel magasin a généré le plus de chiffre d'affaires ?
 def get_store_with_highest_revenue(file_path):
     sales_data = pd.read_csv(file_path)
+    discount_rate = sales_data["Discount"] / 100
+    sales_data["Revenue"] = (
+            sales_data["Units_Sold"]
+            * sales_data["Unit_Price"]
+            * (1 - discount_rate)
+    )
+    top_store = sales_data.groupby("Store")["Revenue"].sum().idxmax()
+    return top_store
 
+
+# 2 Quel genre de livre se vend le mieux en quantité ?
+def most_genre_hav_max_quantity(file_path):
+    sales_data = pd.read_csv(file_path)
+    return sales_data.groupby("Genre")["Units_Sold"].sum().idxmax()
+
+
+# 3 Quel jour de la semaine semble le plus actif (extrapoler avec la date) ?
+def most_actif_week(file_path):
+    sales_data = pd.read_csv(file_path)
+    return sales_data.groupby("Date")["Units_Sold"].sum().idxmax()
+
+
+# 4 Quel livre a la meilleure performance en volume ?
+def most_book_sold_out(file_path):
+    sales_data = pd.read_csv(file_path)
+    return sales_data.groupby("Book_Title")["Units_Sold"].sum().idxmax()
+
+
+# 5 Calculez le revenu total après discount pour chaque transaction
+def revenue_after_discount(file_path):
+    sales_data = pd.read_csv(file_path)
     discount_rate = sales_data["Discount"] / 100
     sales_data["Revenue"] = (
             sales_data["Units_Sold"]
@@ -11,34 +42,21 @@ def get_store_with_highest_revenue(file_path):
             * (1 - discount_rate)
     )
 
-    store_revenue_totals = sales_data.groupby("Store")["Revenue"].sum()
-    top_store = store_revenue_totals.idxmax()
-
-    return top_store
-
-def most_genre_hav_max_quantity(file_path):
-    df = pd.read_csv(file_path)
-    result = df.groupby("Genre")["Units_Sold"].sum().idxmax()
-    return result
+    return sales_data["Revenue"]
 
 
-def most_actif_day(file_path):
-    df = pd.read_csv(file_path)
-    result = df.groupby("Date")["Units_Sold"].sum().idxmax()
-    return result
+# 6 Quel magasin vend le plus de livres pour enfants ?
+def get_store_with_highest_sale_children_book(file_path):
+    children_book = "Children"
+    sales_data = pd.read_csv(file_path)
+    sales_data_children = sales_data[sales_data["Genre"] == children_book]
+    return sales_data_children.groupby("Store")["Units_Sold"].sum().idxmax()
 
 
-def most_book_sold_out(file_path):
-    df = pd.read_csv(file_path)
-    result = df.groupby("Book_Title")["Units_Sold"].sum().idxmax()
-    return result
-
-
-def revenue_after_discount(file_path):
-    df = pd.read_csv(file_path)
-    df.add_prefix("test")
-    print(df)
+def get_book_with_bad_revenue(file_path):
+    sales_data = pd.read_csv(file_path)
+    return sales_data.groupby("Book_Title")["Units_Sold"].sum().idxmin()
 
 
 file_to_test = "E:/data_analyst/work/data/bookstore_sales.csv"
-print(revenue_after_discount(file_to_test))
+print(get_book_with_bad_revenue(file_to_test))
