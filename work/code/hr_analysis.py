e30730809fe97621873519730573a7a5bb9acadd
nassaigael <hei.nassai@gmail.com>
2026-06-12 15:23:30 +0300

    feat(hr_analysis): add functions for age-turnover correlation and salary comparison
    - Added `get_correlation_between_age_and_turnover` to compute the correlation between employee age and turnover, along with average age by status.
    - Added `get_average_salary_between_left_and_right_company` to calculate average salaries based on employee retention and their differences.

--- work/code/hr_analysis.py
+++ work/code/hr_analysis.py
@@ -13,7 +13,19 @@ def get_department_with_highest_rate_turnover(file_path):
         "employee_left"]
     return turnover_by_department["turnover_rate"].sort_values(ascending=False).head(2)
 
+def get_correlation_between_age_and_turnover(file_path):
+    hr_data = pd.read_csv(file_path)
+    hr_data["Left_Binary"] = hr_data["Left_Company"].map({"Yes": 1, "No": 0})
+    correlation = hr_data["Age"].corr(hr_data["Left_Binary"])
+    average_age_by_status = hr_data.groupby("Left_Company")["Age"].mean()
+    return correlation, average_age_by_status
+
+def get_average_salary_between_left_and_right_company(file_path):
+    hr_data = pd.read_csv(file_path)
+    average_salary_by_status = hr_data.groupby("Left_Company")["Monthly_Salary"].mean()
+    average_salary_by_status["diff_between_two"] = average_salary_by_status["Yes"] - average_salary_by_status["No"]
+    return average_salary_by_status
 
 # Usage example
 file_to_test = "E:/data_analyst/work/data/hr_employees.csv"
-print(get_department_with_highest_rate_turnover(file_to_test))
+print(get_average_salary_between_left_and_right_company(file_to_test))
